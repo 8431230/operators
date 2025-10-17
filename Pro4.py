567ccc86ec7f5ce899fb06ba3fa99d95ff91048a
soumya_84 <bangaragundsoumya@gmail.com>
2025-10-17 22:25:03 +0530

    Create Pro4.py

---
+++ Pro4.py
@@ -0,0 +1,10 @@
+a = float("ente 1st number:")
+b = float("ente 2nd number:")
+print("Addition:", a+b)
+print("Subtraction:", a-b)
+print("multiplication:", a*b)
+if b != 0:
+  print("division:", a/b)
+else:
+  print("division: Not possible
+  (Cannot divide by zero)")
